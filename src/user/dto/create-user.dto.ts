e1f756736e78d757249fc3720dd3046a81f80329
Zhanna Chaikovska <zhanna@aerios.app>
2026-04-03 18:25:47 +0300

    fix: provide clearer error messages for invalid user input

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -14,13 +14,20 @@ export class CreateUserDto {
 
   @IsString()
   @IsNotEmpty()
-  @IsStrongPassword({
-    minLength: 8,
-    minLowercase: 1,
-    minUppercase: 1,
-    minNumbers: 1,
-    minSymbols: 1,
-  })
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+
+      minSymbols: 1,
+    },
+    {
+      message:
+        'password must be at least 8 characters long, containing at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special symbol.',
+    },
+  )
   password: string;
 
   @IsEnum(UserRole)
--- src/user/dto/update-password.dto.ts
+++ src/user/dto/update-password.dto.ts
@@ -7,12 +7,18 @@ export class UpdatePasswordDto {
 
   @IsString()
   @IsNotEmpty()
-  @IsStrongPassword({
-    minLength: 8,
-    minLowercase: 1,
-    minUppercase: 1,
-    minNumbers: 1,
-    minSymbols: 1,
-  })
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'newPassword must be at least 8 characters long, containing at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special symbol.',
+    },
+  )
   newPassword: string;
 }
