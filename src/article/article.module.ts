98320423b64dfce7d6454812135af017509540e7
Zhanna Chaikovska <zhanna@aerios.app>
2026-04-12 23:08:30 +0300

    feat: replace Article in-memory operations with Prisma Client queries

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
   controllers: [ArticleController],
   providers: [ArticleService],
   exports: [ArticleService],
+  imports: [DbModule],
 })
 export class ArticleModule {}
